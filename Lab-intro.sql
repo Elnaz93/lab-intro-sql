00f263d847a9849798a8427aa40b3ce5e5e375f4
Elnaz Allahbakhshi <elnaz.allahbakhshi@blujaysolutions.com>
2022-04-30 16:29:39 +0200

    lab-intro-SQL-update

---
+++ Lab-intro.sql
@@ -0,0 +1,11 @@
+use sakila;
+select * from actor;
+select * from film;
+select * from customer;
+select title from sakila.film;
+select * from language;
+select name as LANGUAGE from sakila.language;
+select count(store_id) from sakila.store ; 
+select * from staff ; 
+select first_name from staff ;
+
